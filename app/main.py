191c3db0e91cdb78c04e77f7490145e37c54d1c3
Mally13 <mallymutuku13@gmail.com>
2024-05-31 22:33:47 +0300

    Small fix

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import os
 import requests
 from dotenv import load_dotenv
 from app.routes import donations, user, donor, recipient, reviews
-from database import donor_collection, recipient_collection, donation_collection, user_collection, review_collection
+from app.database import donor_collection, recipient_collection, donation_collection, user_collection, review_collection
 
 load_dotenv()  # Load environment variables from .env file
 
